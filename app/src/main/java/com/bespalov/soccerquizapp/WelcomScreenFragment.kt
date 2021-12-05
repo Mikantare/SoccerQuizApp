78a392fca1f0129d692e0050e1bd44915daf0551
Mikantare <abespalov33@gmail.com>
2021-12-05 16:05:49 +0300

    Initial commit

---
+++ app/src/main/java/com/bespalov/soccerquizapp/WelcomScreenFragment.kt
@@ -0,0 +1,32 @@
+
+package com.bespalov.soccerquizapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.bespalov.soccerquizapp.databinding.ActivityMainBinding
+import com.bespalov.soccerquizapp.databinding.FragmentWelcomScreenBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+
+ */
+class WelcomScreenFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+       val binding: FragmentWelcomScreenBinding = DataBindingUtil.inflate(
+           inflater, R.layout.fragment_welcom_screen, container, false)
+
+        return binding.root
+    }
+
+
+}
